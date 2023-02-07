2ce166e6a32ffc75e2ec800a437382be741a4e59
Laetitia Bodin <laetitia.bodin@colombbus.org>
2023-02-07 19:38:51 +0100

    chore: edit meta tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,14 @@ export class AppComponent {
   theme: string = "light";
   pos: string = "";
   goLight() {
+    const meta = document.querySelector('meta[name=theme-color]');
+    meta?.setAttribute('content', '#0b090a');
     this.theme = "light";
     return (this.pos = "switcher--light");
   }
   goDark() {
+    const meta = document.querySelector('meta[name=theme-color]');
+    meta?.setAttribute('content', '#e0afa0');
     this.theme = "dark";
     return (this.pos = "switcher--dark");
   }
